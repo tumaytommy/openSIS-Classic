52275d6a0a933f2b039337615238b118ef86d3fc
Anwesha Roy <anwesha@os4ed.com>
2026-02-17 16:59:00 +0530

    Update Prototype.js

--- install/js/Prototype.js
+++ install/js/Prototype.js
@@ -3874,7 +3874,7 @@ Object.extend(Selector, {
 
   split: function(expression) {
     var expressions = [];
-    expression.scan(/(([\w#:.~>+()-]+|\*|\[[^\]]*\])+)\s*(,|$)/, function(m) {
+    expression.scan(/([^,]+)\s*(,|$)/, function(m) {
       expressions.push(m[1].strip());
     });
     return expressions;
