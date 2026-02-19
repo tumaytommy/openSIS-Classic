ed2f56b5e5c1d1823b7cf40fdf3e668722ce1726
Anwesha Roy <anwesha@os4ed.com>
2026-02-19 20:09:41 +0530

    Commit regarding issue #125
    - https://github.com/OS4ED/openSIS-Classic/security/code-scanning/125

--- install/js/Prototype.js
+++ install/js/Prototype.js
@@ -3487,7 +3487,7 @@ Object.extend(Selector, {
       return new Template('n = h.attr(n, r, "#{1}", "#{3}", "#{2}", c); c = false;').evaluate(m);
     },
     pseudo: function(m) {
-      if (m[6]) m[6] = m[6].replace(/"/g, '\\"');
+      if (m[6]) m[6] = m[6].replace(/\\/g, '\\\\').replace(/"/g, '\\"');
       return new Template('n = h.pseudo(n, "#{1}", "#{6}", r, c); c = false;').evaluate(m);
     },
     descendant:   'c = "descendant";',
