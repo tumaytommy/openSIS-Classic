4d5dd42fde6a0acafad77bdc166266784d6ca2ff
Anwesha Roy <anwesha@os4ed.com>
2026-02-24 22:46:49 +0530

    Commit reagarding issue #68
    - https://github.com/OS4ED/openSIS-Classic/pull/386

--- js/DivControl.js
+++ js/DivControl.js
@@ -505,9 +505,20 @@ function toggle_course_weight(param, cp_id) {
     }
 }
 function ListOutputJS() {
-    var location = document.getElementById("search_path").value;
+    var basePath = document.getElementById("search_path").value;
     var lo_search = document.getElementById("LO_search").value;
-    document.location.href = location + '&LO_search=' + encodeURIComponent(lo_search).replace(/%20/g, '+');
+    var safePath;
+    try {
+        var url = new URL(basePath, window.location.origin);
+        if (url.origin === window.location.origin) {
+            safePath = url.pathname + url.search;
+        } else {
+            safePath = window.location.pathname;
+        }
+    } catch (e) {
+        safePath = window.location.pathname;
+    }
+    document.location.href = safePath + '&LO_search=' + encodeURIComponent(lo_search).replace(/%20/g, '+');
 }
 function cleanModal(id) {
     var obj = document.getElementById(id);
