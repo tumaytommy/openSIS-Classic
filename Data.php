6b98679888c08d1aae8edbed260c46162c6fb407
tumay teshaye <tumaytsehaye27@gmail.com>
2026-04-02 13:29:21 -0700

    Add Amharic language support (lang_am.php) and localization updates

---
+++ Data.php
@@ -0,0 +1,8 @@
+<?php 
+$DatabaseType = 'mysqli'; 
+$DatabaseServer = 'localhost'; 
+$DatabaseUsername = 'root'; 
+$DatabasePassword = ''; 
+$DatabaseName = 'opensis_new3'; 
+$DatabasePort = '3306'; 
+?>
